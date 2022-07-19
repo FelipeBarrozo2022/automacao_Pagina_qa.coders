31b61efd4b0047a6cf764449dc186ccca7138f97
wacoders <w.silva@qacoders-treinamento.com>
2022-07-18 21:46:52 -0300

    remove comment home step feature

--- features/step_definitions/home.rb
+++ features/step_definitions/home.rb
@@ -1,11 +1,3 @@
-# Dado('que eu acesse a home page do projeto Qa.Coders') do
-#     home.load
-# end
-
-# Então('devo visualizar a informação {string}') do |titleHomePage|
-#     home.validate_text_homePage(titleHomePage)
-# end
-
 Dado('que eu acesse a home page do projeto Qa.Coders') do
     home.load
 end
